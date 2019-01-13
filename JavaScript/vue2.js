9db5cb3e5c8f7b08d15d124a7666200c0d67da3a
wlgh325 <wlgh325@naver.com>
2019-01-14 00:40:43 +0900

    add router study

--- JavaScript/vue2.js
+++ JavaScript/vue2.js
@@ -1,5 +1,5 @@
-
 var eventBus = new Vue();
+
 // global component
 Vue.component('child-component', {
    template: '<div>하위 컴포넌트 영역입니다. <button v-on:click="showLog">show</button></div>',
@@ -11,9 +11,6 @@ Vue.component('child-component', {
    }
 });
 
-var name = prompt("이름을 입력해주세요");
-
-document.getElementById('app2').innerHTML = name + "님 환영합니다!!";
 
 var app = new Vue({
    el: '#app',
@@ -22,5 +19,10 @@ var app = new Vue({
          alert('전달 받은 값: ' + value);   //alert에서는 console과 다르게 +로 연결해주어야 한다.
 
       });
-   }
+   },
+   cmp1data: 'fff'
 });
+
+var name = prompt("이름을 입력해주세요");
+
+document.getElementById('app2').innerHTML = name + "님 환영합니다!!";
